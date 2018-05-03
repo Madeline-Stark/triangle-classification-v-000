7079d4584d2f8f68443d840ed6b413814c5eeb0b
Madeline Stark <madeline.stark1@gmail.com>
2018-05-03 23:55:21 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
-  # write code here
+  attr_accessor :side_one, :side_two, :side_three, :kind
+
+  def initialize(side_one, side_two, side_three)
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three
+    #@kind = self.kind
+  end
+
+  def kind
+      if side_one + side_two < side_three || side_two + side_three < side_one || side_one + side_three < side_two || side_one <= 0 || side_two <= 0 || side_three <= 0
+        begin
+          raise TriangleError
+        rescue TriangleError => error
+          puts error.message
+      end
+      elsif side_one == side_two && side_one == side_three
+        kind = :equilateral
+      elsif side_one == side_two || side_one == side_three || side_two == side_three
+        kind = :isosceles
+      else
+        kind = :scalene
+      end
+      kind
+    end
+
+      class TriangleError < StandardError
+        def message
+        "All sides must be greater than 0 and any two sides added together must be greater than the third side!"
+        end
+      end
+
 end
