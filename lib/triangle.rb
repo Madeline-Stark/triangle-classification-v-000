7724be0d0fc91d8c9a8517c05d62e8397101305a
Madeline Stark <madeline.stark1@gmail.com>
2018-05-04 00:17:01 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,9 +13,7 @@ class Triangle
       if side_one + side_two <= side_three || side_two + side_three <= side_one || side_one + side_three <= side_two || side_one <= 0 || side_two <= 0 || side_three <= 0
         begin
           raise TriangleError
-        #rescue TriangleError => error
-          #puts error.message
-      end
+        end
       elsif side_one == side_two && side_one == side_three
         kind = :equilateral
       elsif side_one == side_two || side_one == side_three || side_two == side_three
@@ -27,7 +25,7 @@ class Triangle
     end
 
       class TriangleError < StandardError
-  
+
       end
 
 
