d0c3c076fde932be25bbc4cd403c9b5f56db64e4
Madeline Stark <madeline.stark1@gmail.com>
2018-05-04 00:14:25 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,9 +12,9 @@ class Triangle
   def kind
       if side_one + side_two < side_three || side_two + side_three < side_one || side_one + side_three < side_two || side_one <= 0 || side_two <= 0 || side_three <= 0
         begin
-          raise Triangle::TriangleError
-        rescue Triangle::TriangleError => error
-          puts error.message
+          raise TriangleError
+        #rescue TriangleError => error
+          #puts error.message
       end
       elsif side_one == side_two && side_one == side_three
         kind = :equilateral
@@ -27,9 +27,9 @@ class Triangle
     end
 
       class TriangleError < StandardError
-        def message
-        "All sides must be greater than 0 and any two sides added together must be greater than the third side!"
-        end
+        #def message
+        #{}"All sides must be greater than 0 and any two sides added together must be greater than the third side!"
+        #end
       end
 
       #binding.pry
